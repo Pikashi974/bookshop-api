4d410d906ef90653e6cc92c2c518653b5b518861
Jérémy PAYET <the_big_boss97437@hotmail.fr>
2023-08-09 11:12:01 +0400

    Delete lexit/jwt-authentication-bundle

--- config/bundles.php
+++ config/bundles.php
@@ -8,5 +8,4 @@ return [
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
-    Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
 ];
